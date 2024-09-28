9234d254f90112adc50663eaa0a9aed762835dae
George N. Wong <gnwong@ias.edu>
2024-09-28 19:56:33 -0400

    add example of how to load from top level

--- mahakala/__init__.py
+++ mahakala/__init__.py
@@ -18,3 +18,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
+
+
+from mahakala.geodesics import initialize_geodesics_at_camera
+
+__all__ = [
+    "initialize_geodesics_at_camera",
+]
+
---
+++ mahakala/geodesics.py
@@ -0,0 +1,4 @@
+
+
+def initialize_geodesics_at_camera():
+    return None
